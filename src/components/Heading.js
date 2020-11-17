d9d6157f86e14ec2c8e285608a245ead7d1e48c0
Vishvajeet Vasantrao Dhawale <42410523+mivishvajeet@users.noreply.github.com>
2020-11-17 20:55:04 +0530

    Add files via upload

---
+++ src/components/Heading.js
@@ -0,0 +1,51 @@
+import React from 'react';
+import styled from 'styled-components';
+
+const Header = styled.header`
+  max-width: 60rem;
+  margin: 2rem auto;
+  text-align: center;
+`;
+
+const H1 = styled.h1`
+  font-family: 'Roboto', sans-serif;
+  margin-bottom: 1em;
+  color : #355C7D ;
+  font-size : 35px; 
+`;
+
+// const Input = styled.input`
+//   height: 2.5rem;
+//   width: 20rem;
+//   margin-top: 1em;
+//   outline: none;
+//   text-indent: 1em;
+//   font-size: 1em;
+
+//   ::placeholder {
+//     font-size: .8em;
+//   }
+// `;
+
+// const Button = styled.button`
+//   height: 2.5rem;
+//   padding: 0 1em;
+//   outline: none;
+//   cursor: pointer;
+//   background: #222;
+//   border: none;
+//   color: #fff;
+//   font-size: 1em;
+// `;
+
+export const Heading = () => {
+
+  return (
+    <Header>
+      <H1>Unsplash</H1>
+      <p>Infinite scroll in the photo feed using ReactJs</p>
+      
+     
+    </Header>
+  )
+}
